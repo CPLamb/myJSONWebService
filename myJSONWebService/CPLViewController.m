687f30f98fec33ac3f965a542c9e6a429da048d6
Chris Lamb <CPLamb@Pacbell.net>
2013-02-15 11:20:19 -0800

    Simple small project
    Github practise

--- myJSONWebService/CPLViewController.m
+++ myJSONWebService/CPLViewController.m
@@ -17,13 +17,30 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+// per YouTube tutorial at - http://www.youtube.com/watch?v=jh0bHASUnvY
+	
+    NSString *urlString = [NSString stringWithFormat:@"http://ielmo.xtreemhost.com/array.php"];
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSData *data = [NSData dataWithContentsOfURL:url];
+    
+    NSError *error;
+    NSMutableArray *json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+    NSLog(@"%@", [json objectAtIndex:1]);
+    
+    NSString *imageString = [NSString stringWithFormat:@"%@", [json objectAtIndex:0]];
+    NSURL *urlOne = [NSURL URLWithString:imageString];
+    NSData *newData = [NSData dataWithContentsOfURL:urlOne];
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    [imageView setImage:[UIImage imageWithData:newData]];
+    [self.view addSubview:imageView];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated
 }
 
 @end
